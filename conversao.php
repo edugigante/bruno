7f00d4ba3160ebd69c64f10db9c731fcd1e0c31d
Eduardo Guimraes <projetos@gerenteweb.com>
2012-02-13 09:58:31 -0200

    Arquivo inicial
    Signed-off-by: Eduardo Guimraes <projetos@gerenteweb.com>

---
+++ conversao.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+    <title>Teste de Conversão</title>
+</head>
+<body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25705334-1']);
+  _gaq.push(['_setDomainName', 'viseo.com.br']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- Google Code for Teste Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 1019091610;
+var google_conversion_language = "en";
+var google_conversion_format = "2";
+var google_conversion_color = "333333";
+var google_conversion_label = "vSefCIb02AIQmrX45QM";
+var google_conversion_value = 0;
+if (5,00) {
+  google_conversion_value = 5,00;
+}
+/* ]]> */
+</script>
+<script type="text/javascript" src="http://www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/1019091610/?value=5,00&amp;label=vSefCIb02AIQmrX45QM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+Converteu
+</body>
+</html>
